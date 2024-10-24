ff760c6a8ec323b5bfa6c36b6e6f5f353e1eecc2
polianaoliveira15 <105360435+polianaoliveira15@users.noreply.github.com>
2024-10-24 10:37:09 -0300

    ok

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -3,10 +3,18 @@ const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-g
 async function visualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
+    const pessoasConectadas = (dados.total_pessoas_conectadas) / 1e9
+    const pessoasNoMundo = (dados.total_pessoas_mundo) / 1e9
+    const horas = parseInt (dados.tempo_medio)
+    const minutos = Math.round ((dados.tempo_medio - horas) * 100)
+    const porcentagemConectada = ((pessoasConectadas / pessoasNoMundo) * 100).toFixed(2)
+
     const paragrafo = document.createElement ('p')
     paragrafo.classList.add('graficos-container_texto')
-    paragrafo.innerHTML = ` Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente 
-    ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.`
+    paragrafo.innerHTML = ` Você sabia que o mundo tem <span>${pessoasNoMundo} bilhões</span> de pessoas e que aproximadamente 
+    <sapan>${pessoasConectadas} bilhões </span> estão conectadas em alguma rede social e passam em média <span>${horas}</span> horas e <span>${minutos}</span> conectadas.
+    <br> Isso significa que aproximadamente ${porcentagemConectada}% de pessoas estão conectadas em alguma rede social.`
     }
+
 
     visualizarInformacoesGlobais()
